5108892dc24754752dac6bd976103ceff24aca5e
Phong Hai <147498494+Phong-Hai@users.noreply.github.com>
2023-11-06 11:25:15 +0700

    Update patient.py

--- dev/om_hospital/models/patient.py
+++ dev/om_hospital/models/patient.py
@@ -3,8 +3,10 @@ from odoo import api, fields, models
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
-#This is how you create models
+
     name = fields.Char(string='Name')
+    ref = fields.Char(string='Reference')
     age = fields.Integer(string="Age")
-    gender = fields.Selection([{'male', 'Male'}, {'female', 'Female'}], string="Gender")
-#This were add to the models in setting > technical > models > Hospital
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    room = fields.Integer(string="Room")
+    
