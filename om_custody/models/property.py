2755fe8c5f0a7fb60638449b2bad46ff1c830e59
Phong Hai <147498494+Phong-Hai@users.noreply.github.com>
2023-11-10 11:26:17 +0700

    Add files via upload

---
+++ om_custody/models/property.py
@@ -0,0 +1,37 @@
+from odoo import api, fields, models
+
+class Property(models.Model):
+    _name = "property"
+    _description = ("Property")
+
+    custody = fields.Char(string='Custody')
+    image = fields.Image(string='Image')
+
+    ssi = fields.Image(string='Small-size Image')
+    description = fields.Text(string='Description')
+    company = fields.Text(string='Company')
+    product = fields.Text(string='Product')
+    quan = fields.Char(string='Quantity')
+    delivered = fields.Char(string='Delivered')
+
+    ppn = fields.Char(string='Property Name')
+    msi = fields.Char(string='Medium-sized Image')
+    ppf = fields.Char(string='Property Form')
+    sno = fields.Char(string='Serial No')
+    source = fields.Char(string='Source')
+    returned = fields.Char(string='Returned')
+    aqty = fields.Char(string='Available QTY')
+    delivered = fields.Integer(string='Delivered')
+
+    rqdate = fields.Date(string="Requested Date", required=True)
+    rtdate = fields.Date(string="Return Date", required=True)
+    note = fields.Text(string='Note')
+    ref = fields.Char(string='Code')
+    reason = fields.Char(string="Reason")
+    name = fields.Char(string='Name')
+
+    custody_id = fields.Many2one('custody', string='Custody')
+    product_id = fields.Many2one('product.product', string='Product')
+    lsnum_id = fields.Many2one('stock.lot', string='Lot/Serial Number')
+    uom_id = fields.Many2one('uom.uom', string='Unit of Measure')
+    job_id = fields.Many2one('hr.job', string='Job')
